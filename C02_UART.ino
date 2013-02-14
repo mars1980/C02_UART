e1938dcb2015e722db0a4673fff49a41e655aab6
MARS <cm2878@nyu.edu>
2013-02-14 13:29:12 -0500

    added in Adafruit 7 segment backpack library
    changing from 4 dig 7 Seg display to Adafruit's backpack library for
    the 7seg:
    http://learn.adafruit.com/adafruit-led-backpack/0-dot-56-seven-segment-b
    ackpack

--- C02_UART.ino
+++ C02_UART.ino
@@ -1,25 +1,32 @@
 //MH-Z14 CO2 Module from Zhengzhou Winsen Electronics Technology Co., Ltd
-//simple sketch to get sensor readings via UART
+//http://www.adafruit.com/products/878
+//simple sketch to get sensor readings via UART and print them to Adafruit 7 segment backpack
 //Michael Doherty - Bitponics
 //Crys Moore
 //2.7.12
+//update 2.14.12
 
 
 #include <SoftwareSerial.h>
+#include <Wire.h>
+#include "Adafruit_LEDBackpack.h"
+#include "Adafruit_GFX.h"
 
 //using softserial to send/recieve data over analog pins 0 & 1
 SoftwareSerial mySerial(A0, A1); // RX, TX
+Adafruit_7segment matrix = Adafruit_7segment();
 
-byte cmd[9] = {
-  0xFF,0x01,0x86,0x00,0x00,0x00,0x00,0x00,0x79}; 
+byte cmd[9] = {0xFF,0x01,0x86,0x00,0x00,0x00,0x00,0x00,0x79}; 
 char response[9]; 
 
 
 
-void setup() {
+void setup() 
+{
   Serial.begin(9600);
   mySerial.begin(9600);
-
+  matrix.begin(0x70);
+  matrix.setBrightness(10); //0-15
 }
 
 void loop() {
@@ -31,10 +38,11 @@ void loop() {
   int responseHigh = (int) response[2];
   int responseLow = (int) response[3];
   int ppm = (256*responseHigh)+responseLow;
-  Serial.println(ppm,DEC);
+  //Serial.println(ppm,DEC);
+  matrix.print(ppm,DEC);
+  
 
 
   //delay(1000);  
 }
-
 
