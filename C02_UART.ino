f4a31173eb6eb9594fe101ba570280032e07dfeb
MARS <cm2878@nyu.edu>
2013-02-14 17:10:39 -0500

    C02 readings via soft serial printing to .txt file via SD lib
    folded in 'datalogger' sketch from SD library to log C02 readings to a
    .txt file.

--- C02_UART.ino
+++ C02_UART.ino
@@ -1,23 +1,33 @@
 //MH-Z14 CO2 Module from Zhengzhou Winsen Electronics Technology Co., Ltd
 //http://www.adafruit.com/products/878
 //simple sketch to get sensor readings via UART and print them to Adafruit 7 segment backpack
+//Arduino 1.0
 //Michael Doherty - Bitponics
 //Crys Moore
 //2.7.12
 //update 2.14.12
 
 
+
 #include <SoftwareSerial.h>
 #include <Wire.h>
+#include "RTClib.h"
 #include "Adafruit_LEDBackpack.h"
 #include "Adafruit_GFX.h"
+#include <SD.h>
+
 
 //using softserial to send/recieve data over analog pins 0 & 1
 SoftwareSerial mySerial(A0, A1); // RX, TX
 Adafruit_7segment matrix = Adafruit_7segment();
+RTC_DS1307 RTC;
+File dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+
 
 byte cmd[9] = {0xFF,0x01,0x86,0x00,0x00,0x00,0x00,0x00,0x79}; 
 char response[9]; 
+const int chipSelect = 10;
 
 
 
@@ -26,21 +36,85 @@ void setup()
   Serial.begin(9600);
   mySerial.begin(9600);
   matrix.begin(0x70);
+  Wire.begin();
+  RTC.begin();
   matrix.setBrightness(10); //0-15
+
+  if (! RTC.isrunning())
+  {
+    Serial.println("RTC is NOT running!");
+    // following line sets the RTC to the date & time this sketch was compiled
+    RTC.adjust(DateTime(__DATE__, __TIME__));
+  }
+  
+  Serial.print("Initializing SD card...");
+  // make sure that the default chip select pin is set to
+  // output, even if you don't use it:
+  pinMode(10, OUTPUT);
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    return;
+  }
+  Serial.println("card initialized.");
+
 }
+
 
 void loop() {
   //、Read concentration and temperature value of the sensor
   mySerial.write(cmd,9);
   //Format of data returned by subsidiary detector
   mySerial.readBytes(response, 9);
+  DateTime now = RTC.now();
 
   int responseHigh = (int) response[2];
   int responseLow = (int) response[3];
   int ppm = (256*responseHigh)+responseLow;
   //Serial.println(ppm,DEC);
+  //matrix.print(ppm,DEC);
+  /*
+  Serial.print(now.year(), DEC);
+   Serial.print('/');
+   Serial.print(now.month(), DEC);
+   Serial.print('/');
+   Serial.print(now.day(), DEC);
+   Serial.print(' ');
+   Serial.print(now.hour(), DEC);
+   Serial.print(':');
+   Serial.print(now.minute(), DEC);
+   Serial.print(':');
+   Serial.print(now.second(), DEC);
+   Serial.println();
+   */
+   
+   // make a string for assembling the data to log:
+  String dataString = "";
+//  int analogPin = 0;
+//  int sensor = analogRead(analogPin);
+  dataString = String(ppm);
+  dataFile = SD.open("test.txt", FILE_WRITE);
+
+  // if the file is available, write to it:
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
+    // print to the serial port too:
+    Serial.println(dataString);
+  }  
+  // if the file isn't open, pop up an error:
+  else {
+    Serial.println("error opening datalog.txt");
+  } 
 
 
-  //delay(1000);  
 }
+
+
+
+
+
+
 
